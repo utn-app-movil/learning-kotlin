4e7c405827bd300642a7c021b13c0fce4fbf197b
YohelRojas <yohelrg9814@gmail.com>
2024-10-10 17:54:52 -0600

    Update clase1_1.kt
    Actualización de la impresión linea 5

--- clase1_1.kt
+++ clase1_1.kt
@@ -2,7 +2,7 @@ import com.sun.org.apache.xpath.internal.operations.Bool
 
 fun main(args:Array<String>){
 
-    println("Mi primera corrida con Kotlin")
+    println("Mi primera vez ejecutando Kotlin")
     println("-------------------------\n")
 
     //Variables
